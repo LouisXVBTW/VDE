907e4b1df66573a1e5e544b9ab7f056abb4841a6
LouisXV <louisoregan.2001@gmail.com>
2022-02-07 15:52:53 +0000

    readme update

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Vulnerability-Discovery-And-Exploitation
+
+## Week 1
+ - Covers basics of C and gdb 
+ - Some basic termanoligy
--- labs/week 1/week1.c
+++ labs/week 1/week1.c
@@ -2,12 +2,5 @@
 #include<stdlib.h>
 
 int main(){
-    int a=10;
-    int *ip;
-    ip = &a;
-    printf("Address of variable a: %x\n", &a);
-    printf("Address stored in the pointer variable: %x\n", ip);
-    printf("Value of *ip variable: %d\n", *ip);
-    printf("Address of pointer variable: %x\n", &ip);
-
+    printf("Hello Word");
 }
